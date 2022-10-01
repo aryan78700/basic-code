bcb09072c0bc7e3f85fec42e2b33d3e1f9f6490c
Aryan Razz <aryanrazz78700@gmail.com>
2022-10-01 21:24:31 +0530

    sum of two no.

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,12 @@
-sjdfklsczc,ncvcz,cmxvnxm,cv
+#include<stdio.h>
+int main ()
+{
+    int sum, a, b;
+    printf("enter the no a\n ");
+    scanf("%d",&a);
+    printf("enter the no. b\n");
+    scanf("%d",&b);
+    sum= a+b;
+    printf("sum %d",sum);
+return 0;
+}
