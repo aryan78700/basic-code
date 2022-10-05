f89d1acd1ee984e554631794c07d7e1503f2b58b
Aryan Razz <aryanrazz78700@gmail.com>
2022-10-05 11:06:17 +0530

    sum of two no.

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int area, l, b;
+    printf("enter the length",l);
+    scanf("%d",& l);
+    printf("enter the breadth",b);
+    scanf("%d",& b);
+    area = l*b;
+    printf("area of reactangle- %d", area);
+    return 0;
+}
